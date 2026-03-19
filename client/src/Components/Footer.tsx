16d4e43a80242415e37b7c0b1b6f45180eeafc6c
sakthirithan <sakthirithanofficial@gmail.com>
2026-03-19 10:21:54 +0530

    FEAT: created Footer Component

---
+++ client/src/Components/Footer.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+const Footer = () => {
+  return (
+    <>
+    <div className='text-center py-4 text-gray-400 text-sm
+    border-t border-gray-800 mt-24'
+    >
+        <p className=''
+        >
+            Copyright &copy; 2025 AI Website Builder - Sakthi Rithan
+        </p>
+    </div>
+    </>
+  )
+}
+
+export default Footer
